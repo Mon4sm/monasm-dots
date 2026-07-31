ed4c8203d3ef5032ed50a65ab02d10fbbce1a077
Mon4sm <realninzx@gmail.com>
2026-07-31 23:19:08 +0700

    fix: hardcoded username

---
+++ .config/hypr/scripts/set-eww-vars.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+eww update username="$(whoami)"
--- install/install.c
+++ install/install.c
@@ -12,8 +12,8 @@ char git_path[] = "https://github.com/Mon4sm/monasm-dots";
 char *dependency[] = {"awk","basename","bash","brightnessctl","cat","cp","curl","cut","date","echo","eww","fastfetch","fc-cache","fc-list","git","grep","grim","head","hyprctl","hypridle","hyprland","hyprlock","hyprpaper","jq","kitty","loginctl","ls","mkdir","mv","nmcli","nvim","pamixer","pidof","playerctl","ranger","rm","sh","sleep","slurp","socat","stdbuf","systemctl","uptime","wget","wpctl","xargs"};
 char *font_dependency[] = {"JetBrainsMono Nerd Font"};
 char *exe_file[] = {"batest.sh","getvol.sh","menu.sh","wifictl.sh","batico.sh","current-wifi.sh","kb-delay.sh","music.sh","wifi-delay.sh","battery.sh","get_kb.sh","lock.sh","pmusic.sh","workspace.sh","calendar.sh","getnet.sh","menuctl.sh","usrctl.sh","wifi-disconnect.sh","coretemp.sh"};
-char *hypr_file[] = {"/delay-exec.sh","/battery.sh","/kb-switch.sh","/text_animation/anitext.sh","/weather/weather.sh"};
-char *hypr_name[] = {"delay-exec.sh","battery.sh","kb-switch.sh","anitext.sh","weather.sh"};
+char *hypr_file[] = {"/delay-exec.sh","/battery.sh","/kb-switch.sh","/set-eww-vars.sh","/text_animation/anitext.sh","/weather/weather.sh"};
+char *hypr_name[] = {"delay-exec.sh","battery.sh","kb-switch.sh","set-eww-vars.sh","anitext.sh","weather.sh"};
 
 void boot(){
     printf("\033[93mStarting monasm-dots installation script");
