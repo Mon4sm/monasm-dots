df918037f8243c4dfa6f3154727fd67ce584c5f7
Mon4sm <realninzx@gmail.com>
2026-09-05 09:56:50 +0700

    chore: sockets

--- .config/eww/scripts/get_kb.sh
+++ .config/eww/scripts/get_kb.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-HYPRLAND_SIGNATURE_ACTUAL=$(ls -td /run/user/1000/hypr/*/ 2>/dev/null | head -n1 | xargs basename)
+RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+HYPRLAND_SIGNATURE_ACTUAL="${HYPRLAND_INSTANCE_SIGNATURE:-$(ls -td "$RUNTIME_DIR"/hypr/*/ 2>/dev/null | head -n1 | xargs -r basename)}"
+SOCKET="$RUNTIME_DIR/hypr/$HYPRLAND_SIGNATURE_ACTUAL/.socket2.sock"
 
-socat -u UNIX-CONNECT:/run/user/1000/hypr/"$HYPRLAND_SIGNATURE_ACTUAL"/.socket2.sock - |
+socat -u UNIX-CONNECT:"$SOCKET" - |
     stdbuf -o0 awk -F '>>|,' '/^activelayout>>/ { print toupper(substr($3, 1, 2)) }'
--- .config/eww/scripts/workspace.sh
+++ .config/eww/scripts/workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ws() {
+ws(){
     local workspaces=6
     local workspace_icon=()
     local workspace_class=()
@@ -33,8 +33,9 @@ ws() {
     /usr/bin/eww update workspaces-output="$output"
 }
 
-HYPRLAND_SIGNATURE_ACTUAL=$(ls -td /run/user/1000/hypr/*/ | head -n1 | xargs basename)
-SOCKET="/run/user/1000/hypr/${HYPRLAND_SIGNATURE_ACTUAL}/.socket2.sock"
+RUNTIME_DIR="${XDG_RUNTIME_DIR:-/run/user/$(id -u)}"
+HYPRLAND_SIGNATURE_ACTUAL="${HYPRLAND_INSTANCE_SIGNATURE:-$(ls -td "$RUNTIME_DIR"/hypr/*/ 2>/dev/null | head -n1 | xargs -r basename)}"
+SOCKET="$RUNTIME_DIR/hypr/${HYPRLAND_SIGNATURE_ACTUAL}/.socket2.sock"
 
 stdbuf -oL socat -U - UNIX-CONNECT:"$SOCKET" | while read -r line; do
     case $line in
